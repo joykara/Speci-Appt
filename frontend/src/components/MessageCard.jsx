18b04c7510a044cda01e55c31e845294d4b33aea
joykara <joykarani1@gmail.com>
2024-02-12 19:05:36 +0300

    initial commit

---
+++ frontend/src/components/MessageCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// This component displays the error message on screen
+// To display the error, you need to pass errorMessage as prop to this component
+
+const MessageCard = ({
+  message, messageType,
+}) => (
+  <div
+    className={messageType}
+  >
+    <p className="tc pa2">
+      {message}
+    </p>
+  </div>
+);
+
+export default MessageCard;
