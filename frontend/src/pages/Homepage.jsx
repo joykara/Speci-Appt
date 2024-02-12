e9ca865687d1f3476e9f4424b8521a2029756cee
joykara <joykarani1@gmail.com>
2024-02-12 19:51:22 +0300

    create homepage

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-script-url */
+import React, {useEffect} from 'react';
+import { Navbar2, ScrollToTop } from '../components';
+
+
+function Homepage() {
+
+  return (
+    <div>
+      <Navbar2 />
+      <Header />
+      <ScrollToTop />
+    </div>
+  );
+}
+
+export default Homepage;
