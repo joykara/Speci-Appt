1d55957db284e7b81882ac40064b861ef9e08f42
joykara <joykarani1@gmail.com>
2024-02-13 21:16:58 +0300

    edit

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-script-url */
-import React, {useEffect} from 'react';
+import React from 'react';
 import { Navbar, ScrollToTop } from '../components';
 
 
@@ -10,9 +10,6 @@ function Homepage() {
       <Navbar />
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -20,7 +17,6 @@ function Homepage() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Header />
       <ScrollToTop />
     </div>
   );
