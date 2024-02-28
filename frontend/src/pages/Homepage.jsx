d75a729351ea2dd95a205b3d2b60fb7b1b61b34c
joykara <joykarani1@gmail.com>
2024-02-28 11:07:28 +0300

    add styling

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -6,9 +6,16 @@ import { Navbar, ScrollToTop } from '../components';
 function Homepage() {
 
   return (
-    <div>
+    <div className="sp-main">
       <Navbar />
-      
+      <div className="sp-topbar">
+        <h1>Dashboard</h1>
+        {/* current date set as day date month and year */}
+        <h2>{new Date().toLocaleDateString()}</h2>
+        <div>
+          
+        </div>
+      </div>
       <ScrollToTop />
     </div>
   );
