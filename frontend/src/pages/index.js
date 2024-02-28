266851fbcc7e380ff9f93b1fa58227f772caa5f9
joykara <joykarani1@gmail.com>
2024-02-28 11:07:15 +0300

    add components

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,5 @@
 //export branches from pages
 export { default as Homepage } from './Homepage';
-export { default as Login } from './Login';
+export { default as Login } from './login/Login';
 export { default as Profile } from './Profile';
+export { default as SignUp } from './signup/SignUp';
