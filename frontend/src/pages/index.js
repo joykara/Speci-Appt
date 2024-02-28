ba06b1064d85d5ce8847a1b45e119222eee9177d
joykara <joykarani1@gmail.com>
2024-02-28 11:57:27 +0300

    change home page folder

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
 //export branches from pages
-export { default as Homepage } from './Homepage';
+export { default as Homepage } from './home/Homepage';
 export { default as Login } from './login/Login';
 export { default as Profile } from './Profile';
 export { default as SignUp } from './signup/SignUp';
