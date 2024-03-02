41e767a768461c89e0d2530f013ca202c4a47745
joykara <joykarani1@gmail.com>
2024-03-02 09:48:01 +0300

    edit login process

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,23 +1,25 @@
-import React, {useRef, useState} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './login.css';
-// import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn, FaPhoneVolume } from 'react-icons/fa';
-// import { MdLocationOn } from 'react-icons/md';
-// import emailjs from 'emailjs-com';
-// import axios from 'axios';
-// import { BASE_URL } from '../../utils/config';
 
 const Login = () => {
-
-  // Ref for the form
-  const form = useRef();
+  const navigate = useNavigate();
+  const userRef = useRef();
+  const passwordRef = useRef();
 
   // handle submit and set up email.js
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Form submitted!');
-};
 
+    // go to homepage on successful submission
+    if (userRef.current.value === 'admin' && passwordRef.current.value === 'admin') {
+      console.log('Logged in');
+      navigate('/home');
+    } else {
+      console.log('Wrong username or password');
+    }
+  };
 
   return (
     <div className="sp-login">
@@ -26,12 +28,16 @@ const Login = () => {
         <label>Username</label>
         <input className="sp-loginInput" type="text" placeholder="Enter your username..." ref={userRef} />
         <label>Password</label>
-        <input className="sp-loginInput" type="password" placeholder="Enter your password..." ref={passwordRef}/>
-        <button className="light-button" type='submit' >Login</button>
+        <input className="sp-loginInput" type="password" placeholder="Enter your password..." ref={passwordRef} />
+        <button className="light-button" type="submit">
+          Login
+        </button>
       </form>
-        <Link to={'/'}><button className="sp-loginRegisterButton">Sign Up</button></Link>
+      <Link to={'/sign-up'}>
+        <button className="sp-registerButton">Sign Up</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
