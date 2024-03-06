1b95a9729ac09ee2569d6aaa6ce4c8252abec7da
joykara <joykarani1@gmail.com>
2024-03-06 07:06:22 +0300

    implement toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {Homepage , Login, Profile, SignUp} from './pages';
+import { Homepage, Login, Profile, SignUp } from './pages';
+import { Toaster } from 'react-hot-toast';
 
 import './App.css';
 
 function App() {
 
   return (
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<Login />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path="/home" element={<Homepage />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Toaster position="top-right" reverseOrder={false}/>
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   )
 }
 
