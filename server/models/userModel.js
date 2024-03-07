cf612951908c8db98aba60d304044d5569f390e3
joykara <joykarani1@gmail.com>
2024-03-07 12:32:45 +0300

    edit user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true,
         trim: true,
         minlength: 3
     },
@@ -13,32 +12,26 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    phone: {
+    contact: {
         type: String,
         required: true,
         unique: true
     },
     address: {
-        type: String,
-        required: true,
-        unique: true
+        type: String
     },
     dob: {
         type: Date,
-        required: true,
-        unique: true
+        required: true
     },
     password: {
         type: String,
         required: true,
-        unique: true,
         minlength: 3
     }
-    },
-    {
-    timestamps: true,
-    }
-);
+}, {
+    timestamps: true
+});
 
 const userModel = mongoose.model('user', userSchema);
 module.exports = userModel;
