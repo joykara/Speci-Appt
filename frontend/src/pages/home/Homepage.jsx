d02b12376c80d2e737fbc1ac29c1b6c0f2914cfe
joykara <joykarani1@gmail.com>
2024-03-08 22:38:41 +0300

    fetch user data

--- frontend/src/pages/home/Homepage.jsx
+++ frontend/src/pages/home/Homepage.jsx
@@ -1,13 +1,49 @@
 /* eslint-disable no-script-url */
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import './homepage.css';
 import { Navbar, ScrollToTop } from '../../components';
 import { IoMdNotificationsOutline } from 'react-icons/io';
 import { FaUserCircle } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { BASE_URL } from '../../utils/config';
 
+const Homepage = () => {
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState(null);
 
-function Homepage() {
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          // Redirect to login page if token is not available
+          navigate('/login');
+          return;
+        }
 
+        // Fetch user data using the JWT token
+        const response = await axios.post(`${BASE_URL}/user/profile`, {} , {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        console.log(response.data)
+        // Set the user data in state
+        setUserData(response.data);
+      } catch (error) {
+        // Handle errors
+        console.error('Error fetching user data:', error);
+        toast.error('Error fetching user data');
+      }
+    };
+
+    fetchUserData();
+  }, [navigate]);
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
       <div className="sp-main">
@@ -27,7 +63,7 @@ function Homepage() {
           </div>
 
           <div className="sp-header">
-            <h3>Hello Joy</h3>
+            <h3>Hello {userData.username}</h3>
             <p>Welcome to your Dashboard. Quickly view your recent activity</p>
           </div>
 
