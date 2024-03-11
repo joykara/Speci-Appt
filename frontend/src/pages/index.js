5f2d98b26c8a0c5b8c719f33825547428a64816a
joykara <joykarani1@gmail.com>
2024-03-11 09:19:36 +0300

    add appts page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,3 +3,4 @@ export { default as Homepage } from './home/Homepage';
 export { default as Login } from './login/Login';
 export { default as Profile } from './profile/Profile';
 export { default as SignUp } from './signup/SignUp';
+export { default as Appointments } from './appointments/Appointments';
