5d69edafd2117e39f56020d1f7b3c27151d126c0
joykara <joykarani1@gmail.com>
2024-03-11 09:18:28 +0300

    set up loading with splashscreen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Homepage, Login, Profile, SignUp } from './pages';
+import { Homepage, Login, Profile, SignUp, Appointments } from './pages';
 import { Toaster } from 'react-hot-toast';
-
+import { SplashScreen } from './components';
+import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const {loading} = useSelector(state => state.alerts);
   // user authentication
   const isLoggedIn = localStorage.getItem('token');
 
   return (
     <Router>
+      {loading && <SplashScreen />}
       <Toaster position="top-right" reverseOrder={false}/>
       <Routes>
         <Route path="/" element={isLoggedIn ? <Homepage /> : <Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={isLoggedIn ? <Profile /> : <Login />} />
+        <Route path='/appointments' element={<Appointments />} />
       </Routes>
     </Router>
   )
