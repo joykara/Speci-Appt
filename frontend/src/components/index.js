1fff249454e9ed1992041f69f1e1301f785f1762
joykara <joykarani1@gmail.com>
2024-03-11 09:19:11 +0300

    add splashscreen

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,4 @@
 //export branches from components
 export { default as Navbar } from './navbar/Navbar';
 export { default as ScrollToTop } from './scroll/ScrollToTop';
+export { default as SplashScreen } from './splashscreen/SplashScreen';
