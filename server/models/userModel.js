599020f86627fd85a54ad26f3077b9d6f24e5371
joykara <joykarani1@gmail.com>
2024-03-11 09:22:13 +0300

    edit user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -33,5 +33,5 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 });
 
-const userModel = mongoose.model('user', userSchema);
+const userModel = mongoose.model('User', userSchema);
 module.exports = userModel;
