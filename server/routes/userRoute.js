1cd3dcbe01b39de0c1211982eb431eb0996623ab
joykara <joykarani1@gmail.com>
2024-03-19 00:18:21 +0300

    set up admin validation

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -48,9 +48,6 @@ router.post('/login', async (req, res) => {
             return res.status(200).send({ msg: "User does not exist", success: false });
         }
 
-        if (!user.isAdmin) {
-            return res.status(200).send({ msg: "User is not an admin", success: false });
-        }
 
         const validPass = await bcrypt.compare(req.body.password, user.password);
         if (!validPass) {
@@ -62,9 +59,14 @@ router.post('/login', async (req, res) => {
             expiresIn: '1d'
         });
 
+        if (user.isAdmin) {
+            return res.status(200).json({ admin: true, token: token, msg: "Logged in successfully as admin", success: true });
+        }
+        console.log(user.isAdmin)
+
         // Return response with token
         res.cookie('token', token, { httpOnly: true })
-            .json({ token: token, admin: true, msg: "Logged in successfully", success: true });
+            .json({ token: token, msg: "Logged in successfully", success: true });
     } catch (err) {
         console.error("Error logging in:", err);
         res.status(500).json({ msg: "Something went wrong!", success: false });
@@ -93,5 +95,50 @@ router.post('/profile', verifyToken, async (req, res) => {
     }
 });
 
+//  Update Profile
+router.put('/update', verifyToken, async (req, res) => {
+    try {
+        const user = await User.findOne({_id: req.body.userId});
+        if (!user) {
+            return res.status(404).json({ msg: "User not found", success: false });
+        }
+
+        // Fields to be updated
+        let updateData = {};
+        if (req.body.username !== "") updateData.username = req.body.username;
+        if (req.body.password !== "") {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            updateData.password = hashedPassword;
+        }
+        if (req.body.email !== "") updateData.email = req.body.email;
+        if (typeof req.body.contact !== "undefined") updateData.contact = req.body.contact;
+        if (req.body.address !== "") updateData.address = req.body.address;
+        if (req.body.dob !== "") updateData.dob = req.body.dob;
+
+        // Check for existing user with new email and send error if so
+        if (updateData.email && !user.email === updateData.email) {
+            let existingUser = await User.findOne({email: updateData.email});
+            if (existingUser) throw Error("User with that email already exists");
+        }
+
+        await User.updateOne({ _id: req.body.userId }, updateData);
+        res.json({msg: 'Profile updated successfully', success: true});
+    } catch (err) {
+        console.error("Error updating user profile:", err);
+        res.status(500).json({ msg: "Error updating user profile", success: false });
+    }
+});
+
+// Get all Users
+router.get('/all', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({ success: true, data: users });
+    } catch (err) {
+        console.error("Error getting all users:", err);
+        res.status(500).json({ msg: "Error getting all users", success: false });
+    }
+});
 
 module.exports = router;
