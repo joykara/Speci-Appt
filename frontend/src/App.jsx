4b8d4a540a81fd7edd5e0ad646d4cb8a8ae4aab0
joykara <joykarani1@gmail.com>
2024-03-19 00:19:23 +0300

    add doctors and patients page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Homepage, Login, Profile, SignUp, Appointments, ApplyDoctor } from './pages';
+import { Homepage, Login, Profile, SignUp, Appointments, ApplyDoctor, Doctors, Patients } from './pages';
 import { Toaster } from 'react-hot-toast';
 import { SplashScreen } from './components';
 import { useSelector } from 'react-redux';
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   const { loading } = useSelector(state => state.alerts);
   const isLoggedIn = localStorage.getItem('token');
-  const isAdmin = localStorage.getItem('admin') === 'true';
+  const isAdmin = JSON.parse(localStorage.getItem('admin')); // Parse to boolean
 
   return (
     <Router>
@@ -18,13 +18,20 @@ function App() {
         <Route path="/" element={isLoggedIn ? <Homepage /> : <Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={isLoggedIn ? <Profile /> : <Login />} />
-        <Route path="/appointments" element={isLoggedIn ? <Appointments /> : <Login />} />
+
+        {/* Only visible to logged in users (not admin) */}
+        <Route path="/profile" element={isLoggedIn && !isAdmin ? <Profile /> : <Login />} />
+        <Route path="/appointments" element={isLoggedIn && !isAdmin ? <Appointments /> : <Login />} />
+
+        {/* Only visible to logged in admin */}
         <Route
           path="/apply-doctor"
           element={isLoggedIn ? (isAdmin ? <ApplyDoctor /> : <Navigate to="/" />) : <Login />}
         />
+        <Route path="/doctors" element={isLoggedIn ? (isAdmin ? <Doctors /> : <Navigate to="/" />) : <Login />} />
+        <Route path="/users" element={isLoggedIn ? (isAdmin ? <Patients /> : <Navigate to="/" />) : <Login />} />
       </Routes>
+
     </Router>
   );
 }
