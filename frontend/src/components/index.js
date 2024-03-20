f08359a70168e841dba9a5ef4733100530283e26
joykara <joykarani1@gmail.com>
2024-03-20 08:02:32 +0300

    export branches

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,3 +2,6 @@
 export { default as Navbar } from './navbar/Navbar';
 export { default as ScrollToTop } from './scroll/ScrollToTop';
 export { default as SplashScreen } from './splashscreen/SplashScreen';
+export { default as Topbar } from './topbar/Topbar';
+export { default as SelectDoctor } from './select/SelectDoctor';
+export { default as SelectPatient } from './select/SelectPatient';
